e74c4ee89d1a8729f93db8ae72e04a12ed3a427a
John Duff <duff.john@gmail.com>
2017-03-28 16:27:33 -0400

    fix loading bash completion, add known host completion

--- home/bash/03_completion.bash
+++ home/bash/03_completion.bash
@@ -1,5 +1,25 @@
 #!/bin/bash
 
-if [[ -f /usr/local/etc/bash_completion ]]; then
-  . /usr/local/etc/bash_completion
-fi
+for file in /usr/local/etc/bash_completion.d/* ; do
+  source "$file"
+done
+
+# Complete host names from known hosts
+_complete_ssh_hosts ()
+{
+        COMPREPLY=()
+        cur="${COMP_WORDS[COMP_CWORD]}"
+        comp_ssh_hosts=`cat ~/.ssh/known_hosts | \
+                        cut -f 1 -d ' ' | \
+                        sed -e s/,.*//g | \
+                        grep -v ^# | \
+                        uniq | \
+                        grep -v "\[" ;
+                cat ~/.ssh/config | \
+                        grep "^Host " | \
+                        awk '{print $2}'
+                `
+        COMPREPLY=( $(compgen -W "${comp_ssh_hosts}" -- $cur))
+        return 0
+}
+complete -F _complete_ssh_hosts ssh
