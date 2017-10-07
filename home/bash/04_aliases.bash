cb58e9ae742baf93af0052bd03545cc716c29cdd
John Duff <duff.john@gmail.com>
2017-10-07 13:06:52 -0400

    alias for opening unity

--- home/bash/04_aliases.bash
+++ home/bash/04_aliases.bash
@@ -10,6 +10,9 @@ alias bx='bundle exec'
 alias br='bin/rails'
 alias rt='b ruby -Itest'
 
+# unity
+alias unity='open -na Unity --args -force-metal'
+
 if ls --version | grep -q GNU; then
   eval `dircolors ~/.dir_colors`
   alias ls='ls --color=auto'
