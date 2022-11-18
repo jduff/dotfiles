95846c807a497db40169524cfec19e1c12e5c1f0
John Duff <duff.john@gmail.com>
2022-11-18 14:55:42 -0500

    Exclude git and node_modules with fzf rg

--- home/bash/03_completion.bash
+++ home/bash/03_completion.bash
@@ -21,13 +21,15 @@ fi
 
 # fzf + ag configuration
 if hash fzf 2>/dev/null && hash ag 2>/dev/null; then
-  export FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
+  export FZF_DEFAULT_COMMAND='ag --nocolor --hidden -g "!{node_modules/*,.git/*}"'
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
   export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
   export FZF_DEFAULT_OPTS='
   --color fg:242,bg:236,hl:65,fg+:15,bg+:239,hl+:108
   --color info:108,prompt:109,spinner:108,pointer:168,marker:168
   '
+elif hash fzf 2>/dev/null && hash rg 2>/dev/null; then
+  export FZF_DEFAULT_COMMAND='rg --files --hidden -g "!{node_modules/*,.git/*}"'
 fi
 
 # Complete host names from known hosts
